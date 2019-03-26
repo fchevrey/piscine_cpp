5a00d0b95a9207deb5e35bc160aa7d0aedf8e38e
Fabien CHEVREY <fchevrey@student.42.fr>
2019-03-26 22:05:57 +0100

    beginex02

---
+++ d02/ex02/Fixed.class.cpp
@@ -0,0 +1,136 @@
+#include "Fixed.class.hpp"
+#include <iostream>
+#include <cmath>
+
+const int Fixed::_bits = 8;
+
+Fixed::Fixed(void)
+{
+	_fixed = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int value)
+{
+	_fixed = value << _bits;
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float value)
+{
+	_fixed = (int)roundf(value * (1 << _bits));
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+float		Fixed::toFloat(void) const
+{
+	return (((float)this->_fixed) / (1 << this->_bits));//toFloat
+}
+
+int			Fixed::toInt(void) const
+{
+	return ((this->_fixed >> _bits));
+}
+
+int			Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_fixed);
+}
+void		Fixed::setRawBits(int const raw)
+{
+	this->_fixed = raw;
+	std::cout << "setRawBits member function called" << std::endl;
+}
+
+Fixed &	Fixed::operator=(Fixed const & rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->_fixed = rhs.getRawBits();
+	return (*this);
+}
+
+std::ostream &	operator<<(std::ostream &os, Fixed const & rhs)
+{
+	os << rhs.toFloat();
+	return (os);
+}
+
+Fixed			Fixed::&operator=(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator<(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator>(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator>=(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator<=(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator==(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator!=(Fixed const & rhs)
+{
+}
+
+
+Fixed			Fixed::&operator+(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator-(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator*(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator/(Fixed const & rhs)
+{
+}
+
+Fixed			Fixed::&operator++()
+{
+}
+
+Fixed			Fixed::operator++(int value)
+{
+}
+
+Fixed			Fixed::&operator--()
+{
+}
+
+Fixed			Fixed::operator--(int value)
+{
+}
+
+Fixed	Fixed::&min(const Fixed &a, const Fixed &b)
+{
+}
+
+Fixed	Fixed::&max(const Fixed &a, const Fixed &b)
+{
+}
---
+++ d02/ex02/Fixed.class.hpp
@@ -0,0 +1,45 @@
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+#include <iostream>
+
+class Fixed
+{
+	public :
+		Fixed(void);
+		Fixed(int value);
+		Fixed(float value);
+		Fixed(Fixed const & src);
+		~Fixed(void);
+		int			getRawBits(void) const;
+		void		setRawBits(int const raw);
+		float		toFloat(void) const;
+		int			toInt(void) const;
+
+		Fixed			&operator=(Fixed const & rhs);
+		Fixed			&operator<(Fixed const & rhs);
+		Fixed			&operator>(Fixed const & rhs);
+		Fixed			&operator>=(Fixed const & rhs);
+		Fixed			&operator<=(Fixed const & rhs);
+		Fixed			&operator==(Fixed const & rhs);
+		Fixed			&operator!=(Fixed const & rhs);
+
+		Fixed			&operator+(Fixed const & rhs);
+		Fixed			&operator-(Fixed const & rhs);
+		Fixed			&operator*(Fixed const & rhs);
+		Fixed			&operator/(Fixed const & rhs);
+
+		Fixed			&operator++();
+		Fixed			operator++(int value);
+		Fixed			&operator--();
+		Fixed			operator--(int value);
+		
+		static Fixed	&min(const Fixed &a, const Fixed &b);
+		static Fixed	&max(const Fixed &a, const Fixed &b);
+
+	private :
+		int					_fixed;
+		static const int	_bits;
+};
+std::ostream	&operator << (std::ostream &os, const Fixed &rhs);
+
+#endif
